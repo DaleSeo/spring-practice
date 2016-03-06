1ee60ed2e68194330812d57ee2d86ad265cb29a2
서대영/Store기술개발팀/SKP <dale.seo@sk.com>
2016-03-06 15:03:43 +0900

    Spring Security Test

---
+++ src/main/java/seo/dale/spring/security/HelloMessageService.java
@@ -0,0 +1,17 @@
+package seo.dale.spring.security;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloMessageService {
+
+    @PreAuthorize("authenticated")
+    public String getMessage() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return "Hello : " + authentication;
+    }
+
+}
