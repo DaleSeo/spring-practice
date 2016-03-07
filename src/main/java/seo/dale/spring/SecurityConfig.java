03e19f6dc1a7c30de046eeaf278263d76f6194a5
dale0713 <dale.seo@gmail.com>
2016-03-07 22:06:20 +0900

    Spring Security Debug Mode

--- src/main/java/seo/dale/spring/SecurityConfig.java
+++ src/main/java/seo/dale/spring/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import seo.dale.spring.core.security.service.UserService;
 
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
