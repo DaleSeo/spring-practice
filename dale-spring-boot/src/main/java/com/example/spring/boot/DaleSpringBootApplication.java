df344c2c4b5f0c6b22fa33e661dbbc08a87e3a2c
dale0713 <dale.seo@gmail.com>
2016-04-26 23:07:12 +0900

    dale-spring-boot project started

---
+++ dale-spring-boot/src/main/java/com/example/spring/boot/DaleSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.example.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaleSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaleSpringBootApplication.class, args);
+	}
+}
