b99fccd5a6ba2bfff61684c826f385b438077d27
서대영/Store기술개발팀/SKP <dale.seo@sk.com>
2016-06-16 15:13:49 +0900

    ContentCachingRequestWrapperTest

--- dale-spring/src/test/java/springframework/web/util/ContentCachingRequestWrapperTest.java
+++ dale-spring/src/test/java/springframework/web/util/ContentCachingRequestWrapperTest.java
@@ -6,7 +6,6 @@ import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 
 import javax.servlet.ServletInputStream;
-
 import java.nio.charset.StandardCharsets;
 
 import static junit.framework.TestCase.assertTrue;
@@ -36,20 +35,55 @@ public class ContentCachingRequestWrapperTest {
 		ContentCachingRequestWrapper wrapper = new ContentCachingRequestWrapper(request);
 		ServletInputStream inputStream = wrapper.getInputStream();
 
-		// One the inputStream is read,
-		String content_1 = IOUtils.toString(inputStream);
-		System.out.println("#1. Read inputStream : " + content_1);
-		assertEquals(content, content_1);
+		// Read the half
+		byte[] bytes = new byte[40];
+		inputStream.read(bytes);
+		String content_1 = new String(bytes, wrapper.getCharacterEncoding());
+		System.out.println("#1. Read some inputStream : " + content_1);
+		assertTrue(content_1.contains("\"id\": 1"));
 
-		// Nothing to read from the inputStream.
+		// Read the remaining
 		String content_2 = IOUtils.toString(inputStream);
-		System.out.println("#2. Read inputStream : " + content_2);
-		assertTrue(content_2.isEmpty());
+		System.out.println("#2. Read the ramming inputStream : " + content_2);
+		assertTrue(content_2.contains("\"email\": \"Sincere@april.biz\""));
 
 		// Nothing to read from the inputStream
 		String content_3 = IOUtils.toString(wrapper.getContentAsByteArray(), wrapper.getCharacterEncoding());
 		System.out.println("#3. getContentAsByteArray : " + content_3);
 		assertEquals(content, content_3);
+	}
+
+	@Test
+	public void testGetContentAsByteArray2() throws Exception {
+		String content = "\"category=app+game%2Bmusic&productId=P12345&key=val&key=%EA%B0%92\"";
+
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		request.setMethod("POST");
+		request.setContentType("application/x-www-form-urlencoded; charset=UTF-8");
+		request.setContent(content.getBytes());
+		request.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+		ContentCachingRequestWrapper wrapper = new ContentCachingRequestWrapper(request);
+		ServletInputStream inputStream = wrapper.getInputStream();
+
+		wrapper.getParameter("category");
+
+		// Read the half
+		byte[] bytes = new byte[40];
+		inputStream.read(bytes);
+		String content_1 = new String(bytes, wrapper.getCharacterEncoding());
+		System.out.println("#1. Read some inputStream : " + content_1);
+		// assertTrue(content_1.contains("\"id\": 1"));
+
+		// Read the remaining
+		String content_2 = IOUtils.toString(inputStream);
+		System.out.println("#2. Read the ramming inputStream : " + content_2);
+		// assertTrue(content_2.contains("\"email\": \"Sincere@april.biz\""));
+
+		// Nothing to read from the inputStream
+		String content_3 = IOUtils.toString(wrapper.getContentAsByteArray(), wrapper.getCharacterEncoding());
+		System.out.println("#3. getContentAsByteArray : " + content_3);
+		// assertEquals(content, content_3);
 	}
 
 }
