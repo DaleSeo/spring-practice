a1da654532974571b4238ba47992ceadd320c5e2
Dale Seo <dale.seo@gmail.com>
2016-12-05 15:21:08 +0900

    ReflectionUtilsTest

--- src/test/java/seo/dale/spring/util/ReflectionUtilsTest.java
+++ src/test/java/seo/dale/spring/util/ReflectionUtilsTest.java
@@ -6,15 +6,59 @@ import org.springframework.util.ReflectionUtils;
 public class ReflectionUtilsTest {
 
 	@Test
-	public void test() {
+	public void testDoWithLocalFields() {
 		ReflectionUtils.doWithLocalFields(Sample.class, System.out::println);
 	}
 
-	static class Sample {
+	@Test
+	public void testDoWithLocalMethods() {
+		Sample sample = new Sample();
 
-		long id;
-		String name;
+		ReflectionUtils.doWithLocalMethods(Sample.class, method -> {
+			if (method.getName().startsWith("setId")) {
+				ReflectionUtils.invokeMethod(method, sample, 1);
+			}
+			if (method.getName().startsWith("setName")) {
+				ReflectionUtils.invokeMethod(method, sample, "ABC");
+			}
+		});
 
+		ReflectionUtils.doWithLocalMethods(Sample.class, method -> {
+			if (method.getName().startsWith("get")) {
+				Object value = ReflectionUtils.invokeMethod(method, sample);
+				System.out.println(value);
+			}
+		});
+	}
+
+	public static class Sample {
+
+		private long id;
+		private String name;
+
+		public Sample() {
+		}
+
+		public Sample(long id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		public long getId() {
+			return id;
+		}
+
+		public void setId(long id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
 	}
 
 }
